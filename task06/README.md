d20fc7dbb344a11e851a5dba9234aa43478dd031
hego819 <sasarohego@gmail.com>
2024-05-28 16:34:16 +0900

    task06 finished

--- task06/README.md
+++ task06/README.md
@@ -40,7 +40,7 @@ The program output the computation time. Fill the table below to compare the tim
 
 | brute force | BVH    |
 | ----------- | ------ |
-| ??? ms      | ??? ms |
+| 2214 ms      | 31 ms |
 
 
 
--- task06/main.cpp
+++ task06/main.cpp
@@ -51,6 +51,13 @@ auto sample_hemisphere(
 
   // For Problem 4, write some code below to sample hemisphere with cosign weight
   // (i.e., the sampling frequency is higher at the top)
+  float cos_theta = std::sqrt(unirand.x());
+  float sin_theta = std::sqrt(1 - cos_theta * cos_theta);
+  dir_loc = Eigen::Vector3f(
+      sin_theta * std::cos(phi),
+      sin_theta * std::sin(phi),
+      cos_theta);
+  pdf = cos_theta / float(M_PI);
 
 
   // end of Problem 4. Do not modify the two lines below
@@ -122,11 +129,33 @@ void search_collision_in_bvh(
 
   if (bvhnodes[i_bvhnode].is_leaf()) { // this is leaf node
     const unsigned int i_tri = bvhnodes[i_bvhnode].i_node_left;
+    const auto res = ray_triangle_intersection(ray_org, ray_dir, i_tri, tri2vtx, vtx2xyz);
+    if (!res) return;
+    const auto& [q0,n0] = res.value();
+    const float depth = (q0 - ray_org).dot(ray_dir);
+    if (hit_depth > depth) {
+      is_hit = true;
+      hit_depth = depth;
+      hit_pos = q0;
+      hit_normal = n0;
+    }
     // do something
   } else { // this is branch node
     unsigned int i_node_right = bvhnodes[i_bvhnode].i_node_right;
     unsigned int i_node_left =bvhnodes[i_bvhnode].i_node_left;
     // do something (hint recursion)
+    if (bvhnodes[i_node_right].intersect_bv(ray_org, ray_dir)) {
+      search_collision_in_bvh(
+        is_hit, hit_depth, hit_pos, hit_normal,
+        i_node_right,
+        ray_org, ray_dir, tri2vtx, vtx2xyz, bvhnodes);
+    }
+    if (bvhnodes[i_node_left].intersect_bv(ray_org, ray_dir)) {
+      search_collision_in_bvh(
+        is_hit, hit_depth, hit_pos, hit_normal,
+        i_node_left,
+        ray_org, ray_dir, tri2vtx, vtx2xyz, bvhnodes);
+    }
   }
 }
 
@@ -143,18 +172,18 @@ auto find_intersection_between_ray_and_triangle_mesh(
   Eigen::Vector3f hit_normal;
 
   // for Problem 2,3,4, comment out from here
-  for (unsigned int i_tri = 0; i_tri < tri2vtx.rows(); ++i_tri) {
-    const auto res = ray_triangle_intersection(ray_org, ray_dir, i_tri, tri2vtx, vtx2xyz);
-    if (!res) { continue; }
-    const auto& [q0,n0] = res.value();
-    const float depth = (q0 - ray_org).dot(ray_dir);
-    if (hit_depth > depth) {
-      is_hit = true;
-      hit_depth = depth;
-      hit_pos = q0;
-      hit_normal = n0;
-    }
-  }
+  // for (unsigned int i_tri = 0; i_tri < tri2vtx.rows(); ++i_tri) {
+  //   const auto res = ray_triangle_intersection(ray_org, ray_dir, i_tri, tri2vtx, vtx2xyz);
+  //   if (!res) { continue; }
+  //   const auto& [q0,n0] = res.value();
+  //   const float depth = (q0 - ray_org).dot(ray_dir);
+  //   if (hit_depth > depth) {
+  //     is_hit = true;
+  //     hit_depth = depth;
+  //     hit_pos = q0;
+  //     hit_normal = n0;
+  //   }
+  // }
   // comment out end
 
   // do not edit from here
@@ -208,7 +237,7 @@ int main() {
         img_data_nrm[(ih * img_width + iw) * 3 + 1] = nrm.y() * 0.5f + 0.5f;
         img_data_nrm[(ih * img_width + iw) * 3 + 2] = nrm.z() * 0.5f + 0.5f;
       }
-      continue; // comment out here for Problem 3,4
+      // continue; // comment out here for Problem 3,4
       //
       if (res) { // ambient occlusion computation
         const unsigned int num_sample_ao = 100;
@@ -220,7 +249,7 @@ int main() {
           const auto res1 = find_intersection_between_ray_and_triangle_mesh(
               pos0, dir, tri2vtx, vtx2xyz, bvhnodes);
           if (!res1) { // if the ray doe not hit anything
-            sum += 1.f; // Problem 3: This is a bug. write some correct code (hint: use `dir.dot(nrm)`, `pdf`, `M_PI`).
+            sum += dir.dot(nrm) / (pdf * M_PI); // Problem 3: This is a bug. write some correct code (hint: use `dir.dot(nrm)`, `pdf`, `M_PI`).
           }
         }
         img_data_ao[ih * img_width + iw] = sum / float(num_sample_ao); // do not change
